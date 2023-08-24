44c93a934a796a0de5601b920c5c56a1b76499e8
vannhat0511 <116283925+vannhat0511@users.noreply.github.com>
2023-08-24 11:02:19 +0700

    push home page 2

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+
+/**
+ * PRELOAD
+ * 
+ * loading will be end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+
+
